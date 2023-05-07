5b050b251c9ac4d380c2b57d3f5e1109435cb4e1
OrderLightXander <37125561+OrderLightXander@users.noreply.github.com>
2023-05-07 17:06:17 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,87 +1,74 @@
-import tensorflow as tf
+import cv2
 import numpy as np
-import pyautogui
-import time
-import win32api
-import win32con
+import keyboard
+from keras.models import Sequential
+from keras.layers import Dense, Flatten, Conv2D, MaxPooling2D
+from keras.utils import to_categorical
+from PIL import ImageGrab
 
-# Define the input shape
-input_shape = (224, 224, 3)
+# Define the resources that you want to identify
+resources = ['fiber', 'stone', 'ore', 'wood']
 
-# Define the learning rate
-learning_rate = 0.001
+# Load the training images and labels
+X_train = []
+y_train = []
+for resource in resources:
+    for i in range(8):
+        img = cv2.imread(f'{resource}{i+1}.png', cv2.IMREAD_GRAYSCALE)
+        X_train.append(img)
+        y_train.append(resources.index(resource))
 
-# Define the batch size
-batch_size = 32
+# Convert the training data to numpy arrays and normalize the pixel values
+X_train = np.array(X_train) / 255.0
+y_train = to_categorical(y_train)
 
-# Define the number of epochs
-num_epochs = 10
+# Define the neural network model
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(128, 128, 1)))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dense(len(resources), activation='softmax'))
 
-# Define the flag to switch between learning and bot phases
-learn_mode = True
+# Compile the model and train it on the training data
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.fit(X_train, y_train, epochs=10, batch_size=32)
 
-# Create a dictionary to map labels to integers
-label_to_int = {}
+# Define the function to predict the resource in a screenshot and display a rectangle around the predicted resource
+def predict_resource(screenshot):
+    img = cv2.cvtColor(screenshot, cv2.COLOR_RGB2GRAY)
+    img = cv2.resize(img, (128, 128))
+    img = np.expand_dims(img, axis=-1)
+    img = img / 255.0
+    prediction = model.predict(np.array([img]))
+    resource = resources[np.argmax(prediction)]
+    print(f'Resource: {resource}')
+    # Convert the image to binary format and find the contours of the predicted resource
+    gray = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+    ret, thresh = cv2.threshold(gray, 127, 255, 0)
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    for c in contours:
+        x, y, w, h = cv2.boundingRect(c)
+        cv2.rectangle(screenshot, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    return screenshot
 
-# Create a function that takes a screenshot and preprocesses the image
-def preprocess_image():
-    # Take a screenshot
-    screenshot = pyautogui.screenshot()
-    # Get the current position of the mouse
-    mouse_pos = win32api.GetCursorPos()
-    # Convert the image to the RGB format
-    rgb_image = np.array(screenshot)
-    # Draw a red dot at the mouse position
-    rgb_image[mouse_pos[1], mouse_pos[0], :] = [255, 0, 0]
-    # Resize the image to a fixed size
-    resized_image = tf.image.resize(rgb_image, size=(224, 224))
-    # Normalize the pixel values
-    normalized_image = resized_image / 255.0
-    # Add a batch dimension to the image
-    preprocessed_image = tf.expand_dims(normalized_image, axis=0)
-    return preprocessed_image
+# Take a screenshot when the "p" key is pressed and predict the resource in the screenshot
+def on_press():
+    print('reallyp')
+    screenshot = np.array(ImageGrab.grab())
+    predicted_screenshot = predict_resource(screenshot)
+    cv2.imshow('Screenshot', predicted_screenshot)
 
-# Create a neural network
-model = tf.keras.Sequential([
-    tf.keras.layers.Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=input_shape),
-    tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
-    tf.keras.layers.Conv2D(64, kernel_size=(3, 3), activation='relu'),
-    tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
-    tf.keras.layers.Flatten(),
-    tf.keras.layers.Dense(128, activation='relu'),
-    tf.keras.layers.Dropout(0.5),
-    tf.keras.layers.Dense(len(label_to_int), activation='softmax')
-])
+cv2.namedWindow('Screenshot')
+while True:
+    screenshot = np.array(ImageGrab.grab())
+    cv2.imshow('Screenshot', screenshot)
+    key = cv2.waitKey(1)
 
-# Compile the model
-model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=learning_rate),
-              loss='categorical_crossentropy',
-              metrics=['accuracy'])
+    if keyboard.is_pressed('q'):
+        break
+    if keyboard.is_pressed('p'):
+        on_press()
+        print('pressed')
 
-# Define a threshold for the model confidence
-confidence_threshold = 0.8
-
-# Define a loop that collects data, trains the model, and evaluates its accuracy
-while learn_mode:
-    # Check if left mouse button is clicked
-    if win32api.GetKeyState(win32con.VK_LBUTTON) < 0:
-        preprocessed_image = preprocess_image()
-        label = input("Enter the label for the current action: ")
-        # Map the label to an integer
-        label_int = label_to_int.setdefault(label, len(label_to_int))
-        num_actions = len(label_to_int)
-        label_one_hot = np.zeros((1, num_actions))
-        label_one_hot[0, label_int] = 1
-        # Update the output layer of the model to match the number of actions
-        model.layers[-1] = tf.keras.layers.Dense(num_actions, activation='softmax')
-        # Train the model on the data
-        model.train_on_batch(preprocessed_image, label_one_hot)
-    else:
-        # Take a screenshot
-        preprocessed_image = preprocess_image()
-        # Make a prediction
-        prediction = model.predict(preprocessed_image)
-        predicted_label = None
-        predicted_labels = {}
-        for label, index in label_to_int.items():
-            confidence = prediction[0][index
+cv2.destroyAllWindows()
